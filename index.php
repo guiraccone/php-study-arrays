ccbb54470ff58acad3cf261025d4059bfa12c83f
guiraccone <guilherme.raccone@hotmail.com>
2025-10-09 09:22:41 -0300

    view page

--- index.php
+++ index.php
@@ -1,72 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+$books = [
+    [
+        'name' => 'Dune',
+        'author' => 'Frank Herbert',
+        'purchaseUrl' => 'example.com',
+        'releaseYear' => 1965
+    ],
+    [
+        'name' => 'Dune Messiah',
+        'author' => 'Frank Herbert',
+        'purchaseUrl' => 'example.com',
+        'releaseYear' => 1969
+    ],
+    [
+        'name' => 'Yes',
+        'author' => 'Faaaaaaaa',
+        'purchaseUrl' => 'example2.com',
+        'releaseYear' => 2023
 
-</head>
+    ]
 
-<body>
-
-    <h1>Recommended books </h1>
-
-    <?php
-    $books = [
-        [
-            'name' => 'Dune',
-            'author' => 'Frank Herbert',
-            'purchaseUrl' => 'example.com',
-            'releaseYear' => 1965
-        ],
-        [
-            'name' => 'Dune Messiah',
-            'author' => 'Frank Herbert',
-            'purchaseUrl' => 'example.com',
-            'releaseYear' => 1969
-        ],
-        [
-            'name' => 'Yes',
-            'author' => 'Faaaaaaaa',
-            'purchaseUrl' => 'example2.com',
-            'releaseYear' => 2023
-
-        ]
-
-    ];
-
-    function filter($items, $fn)
-    {
-        $filteredItems = [];
-
-        foreach ($items as $item) {
-            if ($fn($item)) {
-                $filteredItems[] = $item;
-            }
-        }
-
-        return $filteredItems;
-    }
-
-    $filteredItems  = filter($books, function ($book) {
-        return $book['releaseYear'] <= 2000;
-    });
-
-    ?>
+];
 
 
-    <ul>
-        <?php foreach ($filteredItems as $book): ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - by <?= $book['author'] ?>
-                </a>
-            </li>
-
-        <?php endforeach; ?>
-    </ul>
+$filteredItems  = array_filter($books, function ($book) {
+    return $book['releaseYear'] <= 2000;
+});
 
 
-</body>
-
-</html>
+require "index.view.php";
