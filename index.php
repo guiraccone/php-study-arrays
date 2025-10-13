9f11622c6fc72d3669c3f801920c0e6c7071b413
guiraccone <guilherme.raccone@hotmail.com>
2025-10-13 14:40:26 -0300

    pdo lesson and feat: Database class

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 
 require 'functions.php';
 
-require "router.php";
+require "Database.php";
+
+
+$db = new Database();
+$posts = $db->query("select * from posts;");
+
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
