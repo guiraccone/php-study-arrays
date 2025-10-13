965be31e088b7fd193d2fd7f2e8d0300b4defad9
guiraccone <guilherme.raccone@hotmail.com>
2025-10-13 15:30:03 -0300

    feat: config.php and code refactor

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 
 require 'functions.php';
-
+// require "router.php";
 require "Database.php";
 
+$config = require("config.php");
 
-$db = new Database();
-$posts = $db->query("select * from posts;");
+$db = new Database($config['database']);
 
+$posts = $db->query("select * from posts;")->fetchAll();
 
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
