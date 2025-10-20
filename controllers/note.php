470d8da4e643dc5bac668dadd247292dac5967b1
guiraccone <guilherme.raccone@hotmail.com>
2025-10-20 16:49:02 -0300

    introduction to authorization

--- controllers/note.php
+++ controllers/note.php
@@ -5,6 +5,18 @@ $db = new Database($config['database']);
 
 $heading = "Notes";
 
-$note = $db->query("select * from notes where id  = :id;", ['id' =>  $_GET['id']])->fetch();
+$note = $db->query("select * from notes where id = :id;", [
+    'id' =>  $_GET['id']
+])->fetch();
+
+$currentUserId = 1;
+
+if(! $note){
+    abort(Response::NOT_FOUND);
+}
+
+while ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
